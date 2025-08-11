8543819b3d689a61ab174168c3ba3c51cfdf65ee
David Lusseau <31443588+dlusseau@users.noreply.github.com>
2025-08-11 14:05:22 +0200

    Create modularity_coefficient.r

---
+++ modularity_coefficient.r
@@ -0,0 +1,24 @@
+modularity_coefficient<-function(AI,clusass){
+#estimate modularity of each topological modules in a network
+
+#clusass is a vector of size n, with cluster membership for each node (1 to k clusters)
+#AI is a symmetric matrix n x n
+
+n<-dim(AI)[1]
+k<-max(clusass)
+#calculates modularity matrix
+strength<-rowSums(AI)
+rAI<-matrix(0,n,n)
+rAI<-(strength%*%t(strength))/(sum(strength))
+Qq<-AI-rAI  #modularity matrix
+diag(Qq)<-0
+
+mod<-array(0,k)
+
+for (i in 1:k) {
+	mod[i]<-sum(Qq[which(clusass==i),which(clusass==i)])/sum(strength[which(clusass==i)])
+}
+	return(mod)
+}
+
+
